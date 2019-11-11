a98ed48e7c87808b4cdedf37693d7388af3dd5d9
ah <aleiseihernandez@arein.com>
2019-11-11 14:16:36 -0400

    fix

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import serv from "../env";
 
-let socketIO = io(`${serv.url}:${serv.port}`);
+let socketIO = io(`https://counterworld.herokuapp.com/`);
 
 let counterText = document.getElementById("counterText");
 
