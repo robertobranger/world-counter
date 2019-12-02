4b7187721578dfee18769bc231d81187f17c059d
ah <aleiseihernandez@arein.com>
2019-12-02 11:51:14 -0400

    fix

--- env.js
+++ env.js
@@ -2,13 +2,9 @@ let url = "";
 
 if (process.env.ENV === "PRODUCTION") {
   url = "https://counterworld.herokuapp.com/";
-} else if (process.env.ENV === "DEV") {
-  url = "http://127.0.0.1:3000";
 } else {
-  url = "https://counterworld.herokuapp.com/";
+  url = "http://127.0.0.1:3000";
 }
-
-//url = "http://127.0.0.1:3000";
 
 module.exports = {
   url: url,
